e431b264340c71e83ef20985ea0c20dffa92f002
CamiloRojas12 <benitocamelo0002@gmail.com>
2022-02-22 14:05:09 -0500

    subiendo todo el trabajo

---
+++ Karen/Ejercicios/js/1-producto.js
@@ -0,0 +1,33 @@
+"use strict"
+var pera = 20;
+var mora = 30;
+var sandia = 40;
+var coco = 50;
+if (pera ===20)
+{
+console.log("menos costoso")
+}
+else{
+    console.log("Mas costoso")
+}
+if (pera ===30)
+{
+console.log("menos costoso")
+}
+else{
+    console.log("Mas costoso")
+}
+if (pera ===40)
+{
+console.log("menos costoso")
+}
+else{
+    console.log("Mas costoso")
+}
+if (pera ===50)
+{
+console.log("Mas costoso")
+}
+else{
+    console.log("menos costoso")
+}
---
+++ Karen/Ejercicios/js/2-acceso.js
@@ -0,0 +1,12 @@
+"use strict"
+var usuario= "Karen ";
+var contraseña = 1234;
+var usuario_input= "Karen ";
+var contraseña_input = 1234;
+
+if(usuario === usuario_input && contraseña ===contraseña_input)
+{
+console.log("Acceso Aprobado");
+}else{
+    console.log("Acceso denegado");
+}
