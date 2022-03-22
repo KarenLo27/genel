597bd49e1e13b7030b9af4b4bb63d8acc4d01f7d
CamiloRojas12 <benitocamelo0002@gmail.com>
2022-03-22 17:36:38 -0500

    carrito and añadir

--- js/carrito_mercado.js
+++ js/carrito_mercado.js
@@ -1,22 +1,41 @@
 "use strict"
 
 class Automovil{
-    constructor (marca, modelo, anyo){
+    constructor (marca, modelo, anyo, imagen){
            this.marca = marca;
            this.modelo = modelo;
            this.anyo = anyo;
+           this.imagen= imagen;
 
     }
 }
-let auto= new Automovil("Mazda 3", "Touring", 2019);
+let auto= new Automovil("Mazda 3", "Touring", 2019, "img/renault.jpg");
+
 function mostrarVehiculo(){
-    //alert(auto.marca+ " " + auto.modelo);
-    let contenedor = document.createElement(contentMain);
-    let contentMain = document.getElementById("main-content");
+
+    alert(auto.marca+ " " + auto.modelo);
+
+  let contenedor = document.createElement("div");
+  let contentMain = document.getElementById("container");
     contentMain.appendChild(contenedor);
-    
-    //let etiqueta = document.createElement();
-    //let 
 
+  let nodoMarca = document.createElement("label");
+    contenedor.appendChild(nodoMarca);
+  let nodoMarcaTxt= document.createTextNode(auto.marca);
+    nodoMarca.appendChild(nodoMarcaTxt); 
 
+  let nodoModelo = document.createElement("label");
+    contenedor.appendChild(nodoModelo);
+  let nodoModeloTxt= document.createTextNode(auto.modelo);
+   nodoModelo.appendChild(nodoModeloTxt);  
+
+  let nodoAnyo = document.createElement("label");
+   contenedor.appendChild(nodoAnyo);
+  let nodoAnyoTxt= document.createTextNode(auto.anyo);
+  nodoAnyo.appendChild(nodoAnyoTxt);  
+
+  let nodoImagen = document.createElement("img");
+  contenedor.appendChild(nodoImagen);
+  nodoImagen.setAttribute("src","img/renault.jpg");
+  
 }
